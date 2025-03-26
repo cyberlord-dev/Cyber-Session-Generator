02069b2904a29caeff3cb28a02346ac66ba74362
cyberlord-dev <cyberlord127@gmail.com>
2025-03-26 18:39:14 +0100

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,103 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Cyber_Lord,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function CYBER_LORD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Cyber_Lord = Cyber_Lord({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Cyber_Lord.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Cyber_Lord.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Cyber_Lord.sendMessage(Qr_Code_By_Cyber_Lord.user.id, { text: '' + b64data });
+	
+				   let CYBER_LORD_TEXT = `
+┏━━━━━━━━━━━━━━
+┃༒𝐂𝐘𝐁𝐄𝐑 LØRD༒ SESSION IS 
+┃SUCCESSFULLY
+┃CONNECTED ✅🔥
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❶ || Creator = ༒𝐂𝐘𝐁𝐄𝐑 LØRD༒
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❷ || https://whatsapp.com/channel/0029Vb4pakq47Xe8cU8o5e2q
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❸ || Owner = https://wa.me/2348107574599
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❺ || Bot Repo = https://github.com/cyber-lord-dev
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❻ || YouTube = https://www.youtube.com/@Cyberdev2
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+©2024-2099 ༒𝐂𝐘𝐁𝐄𝐑 LØRD༒_`
+	 await Qr_Code_By_Cyber_Lord.sendMessage(Qr_Code_By_Cyber_Lord.user.id,{text:CYBER_LORD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Cyber_Lord.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					CYBER_LORD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await CYBER_LORD_QR_CODE()
+});
+module.exports = router
